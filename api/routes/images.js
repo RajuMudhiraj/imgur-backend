7a696d27019537ec1a04a728318e1a2d7577bd71
RajuMudhiraj <rajuthecodingcreature@gmail.com>
2021-10-28 20:30:15 +0530

    connected to mongodb atlas

--- api/routes/images.js
+++ api/routes/images.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+// Handling GET requests of /images
 router.get('/', (req, res) =>{
     res.status(200).json({
         message: "I am in images router"
--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 
 // requiring and configuring dotenv to access sensitive data using process.env
 require('dotenv').config();
-
 const express = require("express");
 const app = express();
+
+// Connecting to mongodb atlas
+const mongoose = require('mongoose')
+try{
+  
+  const mongoAtlasUri = "mongodb+srv://"+"process.env.MONGO_ATLAS_USER"+":"+"process.env.MONGO_ATLAS_USER"+"@cluster0.anqmb.mongodb.net/imgurbackend?retryWrites=true&w=majority";
+  mongoose.connect(mongoAtlasUri, { useNewUrlParser: true, useUnifiedTopology: true }, () => console.log('Connected to mongodb atlas'));
+}
+catch(err){
+console.log('could\'t connect to database')
+}
 
 
 // parse requests of content-type - application/json
@@ -20,9 +30,13 @@ app.get("/", (req, res) => {
 const imagesRoutes = require('./api/routes/images');
 app.use('/images', imagesRoutes);
 
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+app.listen(PORT, (err) => {
+  if(err){
+    console.log("Error while running server")
+  }
   console.log(`Server is running on port ${PORT}.`);
 });
 
